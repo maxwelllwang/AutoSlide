8b2c9cbc1efe5c0b7c02e0935146e1a5b117029b
maxi479 <maxwelllwang@gmail.com>
2019-12-03 05:41:25 -0600

    basic app

--- app/src/main/java/com/example/autoslide/MainActivity.java
+++ app/src/main/java/com/example/autoslide/MainActivity.java
@@ -1,15 +1,50 @@
 package com.example.autoslide;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Mat;
+import org.opencv.android.JavaCameraView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  CameraBridgeViewBase.CvCameraViewListener2 {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.webClient);
+        WebViewClient MyWebViewClient = new WebViewClient();
+        myWebView.setWebViewClient(MyWebViewClient);
+
+        myWebView.loadUrl("https://cs125.cs.illinois.edu/learn/");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
+
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
     }
 }
